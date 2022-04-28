670f89d2aaa9ff26593868706439f9b0a964ad86
Sunoru <s@sunoru.com>
2022-04-28 11:12:16 -0400

    New IDs.

--- scripts/generate_locales.py
+++ scripts/generate_locales.py
@@ -6,7 +6,6 @@ from urllib.request import urlretrieve
 import yaml
 
 BASE_PATH = os.path.dirname(os.path.dirname(__file__))
-
 LANGUAGES = [
     "chs",
     "cht",
@@ -20,8 +19,17 @@ LANGUAGES = [
     "ko"
 ]
 
+
+class Context:
+    pass
+
+
 def is_cjk(lang):
     return lang in ("chs", "cht", "ja", "jakana", "ko")
+
+
+def to_kebab_case(name):
+    return re.sub(r"\W+|([a-z])([A-Z])", r"\1-\2", name).strip('-').lower()
 
 
 def get_text_resource(lang, tmpdir):
@@ -61,15 +69,16 @@ def find_forme(form_names, forme, species):
             return i
 
 
-def export_pokes(text_data, data, ctx):
-    data["pokemon"] = pokes = {}
-    with open(os.path.join(BASE_PATH, "dex", "pokedex.yaml")) as fi:
-        poke_dex = yaml.safe_load(fi)
+def process_pokes(text_data, ctx):
+    lang = ctx.current_lang
+    print(f"{lang} - pokes")
     base_names = text_data["monsname"]
     form_names = text_data["zkn_form"]
-    lang = ctx["current_lang"]
+    ctx.data[lang]["pokemon"] = pokes = {}
+    keys = ctx.cache.setdefault("pokemon", {})
     if lang == "en":
-        pokemon_names = ctx["pokemon_names"] = {}
+        with open(os.path.join(BASE_PATH, "dex", "pokedex.yaml")) as fi:
+            poke_dex = yaml.safe_load(fi)
         for (key, value) in poke_dex.items():
             # Only import legal Pokemons.
             # All entries missing a tier are illegal.
@@ -89,47 +98,84 @@ def export_pokes(text_data, data, ctx):
                 print(species, file=sys.stderr)
                 continue
             if forme is None:
-                pokes[key] = species
-                pokemon_names[key] = (species_i, 0)
+                keys[key] = (species_i, 0)
                 continue
             forme_i = find_forme(form_names, forme, species)
             if forme_i is None:
                 print((species, forme))
                 raise ValueError()
-            forme = form_names[forme_i]
-            pokes[key] = species if forme == "" else f"{species} ({forme})"
-            pokemon_names[key] = (species_i, forme_i)
-    else:
-        pokemon_names = ctx["pokemon_names"]
-        bracket_left, bracket_right = "（）" if is_cjk(lang) else [" (", ')']
-        for (key, (i0, i1)) in pokemon_names.items():
-            species = base_names[i0]
-            forme = form_names[i1]
-            pokes[key] = species if forme == "" else f"{species}{bracket_left}{forme}{bracket_right}"
-    with open(os.path.join(ctx["output_dir"], lang, "pokemon.yaml"), "w") as fo:
-        yaml.safe_dump(pokes, fo, allow_unicode=True)
+            keys[key] = (species_i, forme_i)
+    bracket_left, bracket_right = "（）" if is_cjk(lang) else [" (", ')']
+    for (key, (i0, i1)) in keys.items():
+        species = base_names[i0]
+        forme = form_names[i1]
+        pokes[key] = species if forme == "" else f"{species}{bracket_left}{forme}{bracket_right}"
     return pokes
 
 
+def process_ids(key, text_data, ctx):
+    lang = ctx.current_lang
+    print(f"{lang} - {key}")
+    ctx.data[lang][key] = ids = {}
+    ctx.data[lang][f"{key}-info"] = descs = {}
+    if key == "ability":
+        names = text_data["tokusei"][1:]
+        info = text_data["tokuseiinfo"][1:]
+    elif key == "item":
+        names = text_data["itemname"]
+        info = text_data["iteminfo"]
+    elif key == "move":
+        names = text_data["wazaname"][1:] + text_data["gwazaname"]
+        info = text_data["wazainfo"][1:] + text_data["gwazainfo"]
+    elif key == "nature":
+        names = text_data["seikaku"][:-1]
+        info = None
+    keys = ctx.cache.setdefault(key, {})
+    if lang == "en":
+        for i, name in enumerate(names):
+            key = to_kebab_case(name)
+            if key == "":
+                continue
+            keys[key] = i
+    for key, i in keys.items():
+        ids[key] = names[i]
+        if info is not None:
+            descs[key] = info[i]
+    return ids, descs
 
-def export_ids(key, text_data, data, ctx):
-    data[key] = ids = {}
-    return ids
+
+def process_common(ctx):
+    lang = ctx.current_lang
+    print(f"{lang} - common")
+    data = ctx.data[lang]
+    common_data = data["common"]
+    pass
+
+
+def export_all(ctx):
+    lang = ctx.current_lang
+    for key, value in ctx.data[lang].items():
+        if len(value) == 0:
+            continue
+        with open(os.path.join(ctx.output_dir, lang, f"{key}.yaml"), "w") as fo:
+            yaml.safe_dump(value, fo, allow_unicode=True)
 
 
 def process(lang, ctx):
-    print(f"Processing {lang}")
-    text_data = get_text_resource(lang, ctx["tmpdir"])
-    output_dir = os.path.join(ctx["output_dir"], lang)
+    print(f"Processing {lang}...")
+    text_data = get_text_resource(lang, ctx.tmpdir)
+    output_dir = os.path.join(ctx.output_dir, lang)
     os.makedirs(output_dir, exist_ok=True)
-    ctx["current_lang"] = lang
-    ctx[lang] = data = {}
-    data["common"] = common_data = {}
-    # export_ids("ability", text_data, data, en_data)
-    # export_ids("item", text_data, data, en_data)
-    # export_ids("move", text_data, data, en_data)
-    # export_ids("nature", text_data, data, en_data)
-    export_pokes(text_data, data, ctx)
+    ctx.current_lang = lang
+    ctx.data[lang] = data = {}
+    data["common"] = {}
+    process_ids("ability", text_data, ctx)
+    process_ids("item", text_data, ctx)
+    process_ids("move", text_data, ctx)
+    process_ids("nature", text_data, ctx)
+    process_common(ctx)
+    process_pokes(text_data, ctx)
+    export_all(ctx)
     return data
 
 
@@ -147,10 +193,11 @@ def main():
     else:
         languages = LANGUAGES.copy()
     assert all(lang in LANGUAGES for lang in languages)
-    ctx = {
-        "output_dir": output_dir,
-        "tmpdir": tmpdir
-    }
+    ctx = Context()
+    ctx.output_dir = output_dir
+    ctx.tmpdir = tmpdir
+    ctx.cache = {}
+    ctx.data = {}
     process("en", ctx)
     for lang in languages:
         if lang == "en":
